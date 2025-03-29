dd03017389e81e866e5480797672d96efd8a5a5f
Cate <dchcpw@protonmail.com>
2025-03-29 09:06:56 -0400

    added planned features

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # UminekoGildedPatcher
 A Python patching tool which enables additional settings in the unofficial Umineko Project PC port.
+
+This patcher is provided under GPL 3, Umineko Project's scripts are under BSD 3.
+
+This tool is provided as-is. Please make backups of your save files.
+
+On Windows, they're here:
+C:\ProgramData\ONScripter-RU\
+
+On MacOS,
+/Library/Mobile Documents/comapple~CloudDocs/ONScripter-RU/
+
+And on Linux they are in:
+~/.onscripter/
+
+If you experience any issues with this patcher, do not reach out to anybody other than me via this GitHub page. If you bother someone else about this I hold special GPL exclusions to hunt you for sport.
--- main.py
+++ main.py
@@ -1,24 +1,76 @@
 import dearpygui.dearpygui as dpg
 
+import webbrowser
+
+# before uploading this get permission from umipro goat & discord guys
+
 def save_callback():
     print("Save Clicked")
 
-dpg.create_context()
-dpg.create_viewport(resizable=False, title="Umineko Gilded Patcher", width=600, height=400)
-dpg.setup_dearpygui()
+def add_and_load_image(image_path, parent=None):
+    width, height, channels, data = dpg.load_image(image_path)
 
-with dpg.window(label="Example Window", no_title_bar=True,width=600, height=400):
-    dpg.add_text("Hello world")
-    dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
+    with dpg.texture_registry() as reg_id:
+        texture_id = dpg.add_static_texture(width, height, data, parent=reg_id)
+
+    if parent is None:
+        return dpg.add_image(texture_id)
+    else:
+        return dpg.add_image(texture_id, parent=parent)
+        
+dpg.create_context()
+dpg.create_viewport(title="Umineko Gilded Patcher", width=800)
+dpg.setup_dearpygui()
+with dpg.window(tag="main_window"):
+    dpg.add_text("Umineko Project: Gilded Patcher (v000)")
+
+    dpg.add_text(" ")
 
     dpg.add_text("What spriteset would you like to use?\n(Note, sprites other than PS3 will require a sizable download.)")
     dpg.add_radio_button(label="spriteSelection", items=["Ryukishi/OG Sprites", "Alchemist/PS3 Sprites", "Panchiko/Mangagamer Sprites"], default_value="Alchemist/PS3 Sprites")
 
-    dpg.add_text("Install Umineko Project GOAT Edition?\nOptional patch by Pteryon on Github.")
-    dpg.add_checkbox(label="umiproGoatSelection", default_value=True)
-    dpg.add_slider_float(label="float")
+    dpg.add_text(" ")
+
+    dpg.add_text("Font scale? (Default is 1.0)\n1.2 is recommended on the Umineko Project website.")
+    dpg.add_input_text(label="Font scale", default_value="1.0", width=100)
+
+    dpg.add_text(" ")
+
+    dpg.add_text("Discord integration? (Provided by m3t4f1v3 on Github)")
+    dpg.add_checkbox(label="Install Discord integration", default_value=True)
+    dpg.add_text(" ")
+
+    
+    dpg.add_text("Enable smallcaps? By default in Umineko Project, BEATRICE will have her name displayed this way.\nIf it matters to you, this was not the case in the original WH/MangaGamer releases.")
+
+    add_and_load_image("res/smallcaps_false.png")
+    add_and_load_image("res/smallcaps_true.png")
+
+    dpg.add_checkbox(label="Smallcaps for BEATRICE", default_value=True)
+
+
+    dpg.add_text(" ")
+    
+
+    dpg.add_text("Would you like your fonts to have a small gradient on them? (Defualt Umineko Project behavior)")
+    add_and_load_image("res/gradient_false.png")
+    dpg.add_checkbox(label="Font gradient", default_value=True)
+
+    dpg.add_text(" ")
+
+    dpg.add_text("What spriteset would you like to use?\n(Note, sprites other than PS3 will require a sizable download.)")
+    add_and_load_image("res/sprites.png")
+    dpg.add_radio_button(label="spriteSelection", items=["Ryukishi/OG Sprites", "Alchemist/PS3 Sprites", "Panchiko/Mangagamer Sprites"], default_value="Alchemist/PS3 Sprites")
+
+    dpg.add_text(" ")
+    
+    dpg.add_text("Install Umineko Project GOAT Edition?\nOptional patch by Pteryon on Github.\nContains new logos, replaces PS3 openings with less-spoilery original PC openings, more.")
+    # note, UMIPROGOAT only provides ps3fication of og openings, need to add original openings
+    dpg.add_button(label="Read more info here!", callback=lambda:webbrowser.open("https://github.com/Pteryon/umipro-goat"))
+    dpg.add_checkbox(label="Install GOAT Edition ", default_value=True)
 
 dpg.show_viewport()
+dpg.set_primary_window("main_window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
+
